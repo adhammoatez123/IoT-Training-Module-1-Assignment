a826e3128436a8cb702bcc3fc7d5e571ac5e768e
Marwan <marwantarek62@gmail.com>
2024-07-22 01:12:42 +0300

    Updated

--- pizza.cpp
+++ pizza.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main()
 {
     float diameter1, diameter2, price1, price2;
-    cout << "Enter the diameter of the first pizza: ";
+    cout << "Enter the diameter (in inches) of the first pizza: ";
     cin >> diameter1;
-    cout << "Enter the diameter of the second pizza: ";
+    cout << "Enter the price (in USD) of the first pizza: ";
+    cin >> price1;
+    cout << "Enter the diameter (in inches) of the second pizza: ";
     cin >> diameter2;
+    cout << "Enter the price (in USD) of the second pizza: ";
+    cin >> price2;
     return 0;
 }
