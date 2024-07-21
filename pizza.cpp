8fe1cce22f0979f6d5f9232d8b18a353661b4e32
Marwan <marwantarek62@gmail.com>
2024-07-22 01:11:02 +0300

    Updated main fn

--- pizza.cpp
+++ pizza.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(){
-    cout<<"Hello World!"<<endl;
+int main()
+{
+    float diameter1, diameter2, price1, price2;
+    cout << "Enter the diameter of the first pizza: ";
+    cin >> diameter1;
+    cout << "Enter the diameter of the second pizza: ";
+    cin >> diameter2;
     return 0;
 }
