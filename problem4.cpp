de05cdeaf9a3535f91c4eb95df07dd889610f8c4
Youssef MO Helmy <161648183+YoussefMoHlemyAlpha@users.noreply.github.com>
2024-07-21 23:15:31 +0300

    Add files via upload
    ِِA program that allows the user to input a starting and an ending temperature. Within this
    temperature range, the program should output the temperature and the corresponding velocity in
    1° increments   by the formula:
    velocity ≈ 331.3 + 0.61 × Tc.

---
+++ problem4.cpp
@@ -0,0 +1,29 @@
+// Module 1 Assignment – Part A – Problem 4
+
+# include <stdio.h>
+
+double velocity;
+
+void calaculate_velocity (){
+
+    int Startpoint,Endpoint;
+    printf("Enter The First Number : \n");
+    scanf("%d",&Startpoint);
+    printf("Enter The First Number : \n");
+    scanf("%d",&Endpoint);
+
+ for (int Tc=Startpoint;Tc<=Endpoint;Tc++){
+  velocity=331.3+0.61*Tc;
+  printf("At %d degrees Celsius the velocity of sound is %.1f m/s \n",Tc,velocity);
+  
+ }
+}
+
+
+int main(){
+
+ calaculate_velocity();
+
+return 0;
+
+}
