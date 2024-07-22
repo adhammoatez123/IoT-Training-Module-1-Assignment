d8fb18b67eecc3bd964d1ad683427040aeb92656
Youssef MO Helmy <161648183+YoussefMoHlemyAlpha@users.noreply.github.com>
2024-07-22 22:38:47 +0300

    Update Bubble Sort.cpp
    Bubble Sort From Scratch

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,1 +1,47 @@
+//Module 1 Assignment – Part A – Double Bonus
+
+
+# include <stdio.h>
+
+void BubbleSort(){
+int size;
+
+
+printf("Please Enter The Size of  Array \n");
+scanf("%d" ,&size);
+int Elements[size];
+
+for (int i=0;i<=size-1;i++){
+    printf("Enter Element %d :",i+1);
+    scanf("%d" ,&Elements[i]);
+} 
+    printf("The Original Array \n");
+    for(int i=0;i<=size-1;i++){
+        printf("%d ",Elements[i],"\n");
+    }
+    
+    int temp;
+    for (int i=0;i<=size-1;i++ ){
+        for (int j=0;j<size-i-1;j++){
+            if (Elements[j]>Elements[j+1]){
+                temp=Elements[j];
+                Elements[j]=Elements[j+1];
+                Elements[j+1]=temp;
+            }
+        }
+    }
+    printf("\n");
+    printf("The Sorted Array \n");
+        for(int i=0;i<=size-1;i++){
+        printf("%d ",Elements[i]);
+    }
+
+}
+int main(){
+
+BubbleSort();
+
+    return 0;
+
+}
 
